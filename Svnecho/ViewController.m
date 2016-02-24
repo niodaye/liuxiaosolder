5a5c35653ae2676cbd0562d8adcb798f2b1b638e
niodaye <291007975@qq.com>
2016-02-24 11:08:23 +0800

    666

--- Svnecho/ViewController.m
+++ Svnecho/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"%@",@"ss696s");
+    NSLog(@"%@",@"ss696gbatjs");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
