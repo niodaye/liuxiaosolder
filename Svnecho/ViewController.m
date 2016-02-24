5ee970377513c4bffdc3b98755a17faf8e264360
niodaye <291007975@qq.com>
2016-02-24 11:26:43 +0800

    htffg

--- Svnecho/ViewController.m
+++ Svnecho/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"%@",@"kkcghgdghfdfgd");
+    NSLog(@"%@",@"scsdf");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
