5a20ad113d534744bf78a1c450d12ec99b0385a9
niodaye <291007975@qq.com>
2016-02-24 14:27:29 +0800

    521

--- Svnecho/ViewController.m
+++ Svnecho/ViewController.m
@@ -19,6 +19,8 @@
     NSLog(@"%@",@"scsdf");
     NSLog(@"%@",@"scsdf");
     NSLog(@"%@",@"scsdf");
+    NSLog(@"%@",@"scsdf");
+    NSLog(@"%@",@"scsdf");
 
     // Do any additional setup after loading the view, typically from a nib.
 }
